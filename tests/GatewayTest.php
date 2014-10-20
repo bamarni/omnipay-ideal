f7eb47e9d2598d484c94a1cd52cb17c096b14681
Bilal Amarni <bilal.amarni@gmail.com>
2014-10-20 11:00:23 +0200

    introduced tests

---
+++ tests/GatewayTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Bamarni\Omnipay\Ideal\Tests;
+
+use Omnipay\GatewayTestCase;
+use Bamarni\Omnipay\Ideal\Gateway;
+
+class GatewayTest extends GatewayTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+        $this->gateway->initialize([
+            'userId'          => '12345',
+            'projectId'       => '54321',
+            'projectPassword' => 'Kilimanjaro',
+            'senderCountryId' => 'NL',
+            'apiKey'          => 'f21f57dpm88700d916ce1c2399',
+            'testMode'        => false,
+        ]);
+    }
+
+    public function testFetchIssuersSuccess()
+    {
+        $this->setMockHttpResponse('FetchIssuersSuccess.txt');
+
+        $response = $this->gateway->fetchIssuers()->send();
+
+        $this->assertInstanceOf('\Bamarni\Omnipay\Ideal\Message\FetchIssuersResponse', $response);
+        $this->assertTrue($response->isSuccessful());
+
+        $expectedIssuers = [
+            'ABNANL2A' => 'ABN Amro',
+            'INGBNL2A' => 'ING',
+        ];
+        $this->assertEquals($expectedIssuers, $response->getIssuers());
+    }
+}
